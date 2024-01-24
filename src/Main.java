a40ecff285f7bcff22df35b20d25009f132a64a2
soymze <soylmze@gmail.com>
2024-01-24 15:00:01 +0300

    asdf

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int time, biggest=0;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Kaç sayı gireceksiniz :");
+        time = sc.nextInt();
+        int nums[] = new int[time];
+
+        for (int i=0; i <time; i++){
+            System.out.println((i+1)+". sayıyı giriniz :");
+            nums[i] = sc.nextInt();
+        }
+
+
+        for (int i=0; i < nums.length-1; i++){
+            if(nums[i] > biggest){
+                biggest = nums[i];
+            }
+        }
+
+        int smallest=nums[1];
+        for (int i=0; i < nums.length-1; i++){
+            if(nums[i+1] < smallest){
+                smallest = nums[i+1];
+            }
+        }
+        System.out.println("En büyük sayı :" +biggest);
+        System.out.println("En küçük sayı :" +smallest);
     }
 }
